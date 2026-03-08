03337b1759cd437fbd8d769d778d88fd1a8c0fc1
mono1it <host1425@gmail.com>
2026-03-09 00:26:17 +0300

    Initial Commit

---
+++ POTD/POTDApp.swift
@@ -0,0 +1,17 @@
+//
+//  POTDApp.swift
+//  POTD
+//
+//  Created by Ilya Shcherbakov on 09.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct POTDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
